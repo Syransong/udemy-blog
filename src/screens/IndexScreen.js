a72c19b5a9da6b5d231340dbf4c19b3414d33ccb
Syransong <ksysong@gmail.com>
2021-07-22 23:19:04 -0700

    add some styling to blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { Text, View, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { AntDesign } from '@expo/vector-icons'; 
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button 
         title="Add Post"
         onPress={addBlogPost} // equivalent to onPress={() => addBlogPost()}
@@ -16,13 +16,34 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost)=> blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <AntDesign style={styles.icon} name="delete"/>
+            </View>
+          )
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray"
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+    color: "black"
+  }
+});
 
 export default IndexScreen;
