1b3afefa6085634991924db0d37de2357b3dcc7d
Syransong <ksysong@gmail.com>
2021-07-23 21:14:17 -0700

    fetch specific blog post when clicked

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity } from "reac
 import { Context } from "../context/BlogContext";
 import { AntDesign } from '@expo/vector-icons'; 
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
@@ -17,13 +17,15 @@ const IndexScreen = () => {
         keyExtractor={(blogPost)=> blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title} - {item.id}</Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-               <AntDesign style={styles.icon} name="delete"/>
-              </TouchableOpacity>
-            </View>
-          )
+            <TouchableOpacity onPress={() => navigation.navigate("Show", { id: item.id})}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <AntDesign style={styles.icon} name="delete"/>
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
