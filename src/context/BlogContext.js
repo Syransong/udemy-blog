c28812bb6e4376fe3ff73eeafef2935c8b947fd3
Syransong <ksysong@gmail.com>
2021-07-26 22:45:40 -0700

    refactor code to delete blogpost from api

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,15 +13,6 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state, 
-        {
-          id: Math.floor(Math.random() * 99999), 
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
     default: 
       return state;
   }
@@ -38,7 +29,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
-    //   dispatch({ type: "add_blogpost", payload: { title, content } });
     
     if (callback) {
       callback();
@@ -47,8 +37,11 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
-    dispatch({ type: "delete_blogpost", payload: id });
+  return async (id) => {
+    // dispatch({ type: "delete_blogpost", payload: id });
+    await jsonServer.delete(`/blogposts/${id}`);
+
+    dispatch({ type: "delete_blogpost", payload: id })
   };
 };
 
